2103ffb0d12e5dd74eb05c9ad9b428ebd2c38c60
Erika Mogollon <fany200025@hotmail.com>
2022-03-19 22:35:10 -0700

    it will pass the fourth test

--- _tests_/planets.test.js
+++ _tests_/planets.test.js
@@ -2,25 +2,30 @@ import Planets from "./../src/js/planets.js";
 
 describe("Planets", () => {
   test("#1 It will create a Planets object", () => {
-    const planets = new Planets(27, "Male");
-    expect(planets.age).toEqual(27);
+    const planets = new Planets(50, "Male");
+    expect(planets.age).toEqual(50);
     expect(planets.gender).toEqual("Male");
   });
   test ("#2 It will return the correct age in Mercury years.", () => {
-    const planets = new Planets(27, "Male");
+    const planets = new Planets(50, "Male");
     planets.lifeExpectancy();
     planets.mercuryAge();
-    expect(planets.mercuryAge).toEqual(6);
-    expect(planets.mercuryLifeExp).toEqual( "Wow you still have 14.64 years. Congrats!! ");
+    expect(planets.mercuryAge).toEqual(12);
   }); 
   test ("#3 It will return the correct age in `Male` in Mercury years.", () => {
-    const planets = new Planets(27, "Male");
+    const planets = new Planets(50, "Male");
     planets.lifeExpectancy();
     planets.mercuryAge();
-    expect(planets.mercuryAge).toEqual(6);
-    expect(planets.mercuryLifeExp).toEqual( "Wow you still have 14.64 years. Congrats!! ");
-    });
-
+    expect(planets.mercuryAge).toEqual(12);
+    expect(planets.mercuryLifeExp).toEqual("Wow my calculations conclude you have gone over by almost -1.6799999999999997 years to be exact. Congrats!! ");
+  });
+  test ("#4 It will return the correct age in `Female` in Mercury years.", () => {
+    const planets = new Planets(50, "Female");
+    planets.lifeExpectancy();
+    planets.mercuryAge();
+    expect(planets.mercuryAge).toEqual(12);
+    expect(planets.mercuryLifeExp).toEqual( "You are lucky to be alive since you have exceeded your life expectancy by 2 years! ");
+  });
 
     //now we want to test that if you are older it will say your years left
   // test ("#3 If mercuryAge < lifeExpectancy it will return lifeExpectancy - mercuryAge", () => {
--- src/js/planets.js
+++ src/js/planets.js
@@ -1,21 +1,50 @@
 export default class Planets {
-  constructor(age, gender) {
+  constructor(age, gender, planet) {
     this.age = age;
     this.gender = gender;
+    this.planet = planet;
+    this.mercury = 0.24;
+    this.venus = 0.62;
+    this.mars = 1.88;
+    this.jupiter = 11.68
   }
   lifeExpectancy() {
     if (this.gender==="Male") {
-      this.lifeExp = 86;
+      this.lifeExp = 43;
     } else if (this.gender ==="female") {
-      this.lifeExp = 89;
+      this.lifeExp = 49;
     }
   }
   mercuryAge() {
     this.mercuryAge = Math.round(this.age * 0.24);
     if (this.mercuryAge < this.lifeExp) {
-      this.mercuryLifeExp = `Wow you still have ${(this.lifeExp * 0.24) - this.mercuryAge} years. Congrats!! `
+      this.mercuryLifeExp = `Wow my calculations conclude you have gone over by almost ${(this.lifeExp * this.mercury) - this.mercuryAge} years to be exact. Congrats!! `
     } else {
-    this.mercuryLifeExp = `Your estimated life here in Mercury is approximately 80 years!`
+    this.mercuryLifeExp = `You are lucky to be alive since you have exceeded your life expectancy by ${this.mercuryAge - 10} years! `
+    };
+  }
+  venusAge() {
+    this.venusAge = Math.round(this.age * 0.62);
+    if (this.venusAge < this.lifeExp) {
+      this.venusLifeExp = `Wow you still have ${(this.lifeExp * this.venus) - this.venusAge} years. Congrats!! `
+    } else {
+    this.venusLifeExp = `You are lucky to be alive since you have exceeded your life expectancy by `
+    };
+  }
+  marsAge() {
+    this.marsAge = Math.round(this.age * 1.88);
+    if (this.marsAge < this.lifeExp) {
+      this.marsLifeExp = `Wow you still have ${(this.lifeExp * this.mars) - this.marsAge} years. Congrats!! `
+    } else {
+    this.marsLifeExp = `You are lucky to be alive since you have exceeded your life expectancy by `
+    };
+  }
+  jupiterAge() {
+    this.jupiterAge = Math.round(this.age * 11.68);
+    if (this.jupiterAge < this.lifeExp) {
+      this.jupiterLifeExp = `Wow you still have ${(this.lifeExp * this.jupiter) - this.jupiterAge} years. Congrats!! `
+    } else {
+    this.jupiterLifeExp = `You are lucky to be alive since you have exceeded your life expectancy by `
     };
   }
 }  
